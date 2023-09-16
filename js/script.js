26ff6cfffd1cbe72d7b9def75bedec52f0f94ab9
Ian Gabriel <i.gabrielgeneralao@gmail.com>
2023-09-16 19:49:09 -0400

    Add an erase and draw functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container");
+
+let isPressed = false;
 function setGrid(size){
     for (let i = 0; i < size; i++){
         for (let j = 0; j < size; j++){
@@ -9,13 +11,24 @@ function setGrid(size){
         box.style.margin = "0";
         box.style.flex = "1";
         container.append(box);
-        box.addEventListener('mouseover', e => {
-            box.style.backgroundColor = "black";
-        })
+        container.addEventListener('mousedown', e => isPressed = true)
+        container.addEventListener('mouseup', e => isPressed = false)
+        container.addEventListener('mouseover', e => {
+            if(isPressed){
+                e.target.style.backgroundColor = color;
+            }
+        });
         }
-
-
     }
 }
+let color = "black";
+const erase = document.querySelector(".erase");
+const draw = document.querySelector(".draw");
+erase.addEventListener("click", e => {
+    color = "white";
+});
 
+draw.addEventListener("click", e => {
+    color = "black";
+});
 setGrid(18);
