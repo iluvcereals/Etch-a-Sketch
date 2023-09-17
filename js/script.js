e0cf42e926ff47e6109bf2571421911e469e1884
Ian Gabriel <i.gabrielgeneralao@gmail.com>
2023-09-16 21:12:48 -0400

    Add reset functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
 
-let isPressed = false;
+
+
 function setGrid(size){
     for (let i = 0; i < size; i++){
         for (let j = 0; j < size; j++){
@@ -8,27 +9,37 @@ function setGrid(size){
         box.style.minHeight = "40px";
         box.style.minWidth = "40px";
         box.style.border = "1px solid black";
-        box.style.margin = "0";
         box.style.flex = "1";
         container.append(box);
-        container.addEventListener('mousedown', e => isPressed = true)
-        container.addEventListener('mouseup', e => isPressed = false)
-        container.addEventListener('mouseover', e => {
-            if(isPressed){
-                e.target.style.backgroundColor = color;
-            }
-        });
+        box.addEventListener('mouseover', startDraw);
         }
     }
 }
+setGrid(18);
+
+
 let color = "black";
 const erase = document.querySelector(".erase");
 const draw = document.querySelector(".draw");
-erase.addEventListener("click", e => {
-    color = "white";
-});
+const reset = document.querySelector(".reset");
+
+reset.addEventListener("click", resetGrid);
+erase.addEventListener("click", eraseColor);
 
 draw.addEventListener("click", e => {
     color = "black";
 });
-setGrid(18);
+
+function eraseColor(){
+    color = "white";
+}
+function startDraw(){
+    this.style.backgroundColor = color;
+}
+
+function resetGrid(){
+    let divs = container.querySelectorAll("div");
+    divs.forEach((div) => {
+        div.style.backgroundColor = "white";
+    })
+}
