24291e8cba9b83cb4604fa0b7d66cb5277d56529
Ian Gabriel <i.gabrielgeneralao@gmail.com>
2023-09-18 17:14:35 -0400

    Add a resizable grid

--- js/script.js
+++ js/script.js
@@ -1,70 +1,26 @@
 const container = document.querySelector(".container");
-let click = false;
 
+function createGrid(size){
+    for(let i = 0; i < size; i++){
+        createRow(size);
+    }
+    
+}
 
-
-function setGrid(size){
+function createRow(size){
     for (let i = 0; i < size; i++){
+        let div = document.createElement("div");
+        container.append(div);
+        div.style.border = "1px solid red";
         for (let j = 0; j < size; j++){
-        let box = document.createElement("div");
-        box.style.minHeight = "40px";
-        box.style.minWidth = "40px";
-        box.style.border = "1px solid transparent";
-        box.style.flex = "1";
-        container.append(box);
-        box.addEventListener('mouseover', startDraw);
+            let box = document.createElement("div");
+            div.append(box);
+            box.style.border = "1px solid black";
+            box.style.flexDirection = "f;e";
         }
     }
-}
-setGrid(18);
 
-
-let color = "black";
-const erase = document.querySelector(".erase");
-const draw = document.querySelector(".draw");
-const reset = document.querySelector(".reset");
-const random = document.querySelector(".random");
-
-reset.addEventListener("click", resetGrid);
-erase.addEventListener("click", eraseColor);
-
-draw.addEventListener("click", e => {
-    color = "black";
-});
-
-random.addEventListener("click", randomize)
-
-function eraseColor(){
-    color = "white";
-}
-function startDraw(){
-   if(click){
-    if (color === "random"){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    } else {
-        this.style.backgroundColor = color;
-    }
-   }
+    
 }
 
-function randomize(){
-    color = "random";
-}
-
-function resetGrid(){
-    let divs = container.querySelectorAll("div");
-    divs.forEach((div) => {
-        div.style.backgroundColor = "white";
-    })
-}
-
-
-container.addEventListener("click", e => {
-    click = !click;
-})
-
-container.addEventListener("mouseleave", e => {
-    click = false;
-})
-
-
+createRow(18)
