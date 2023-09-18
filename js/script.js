d83d0b3a28abe25462e6d16df492c69e67668883
Ian Gabriel <i.gabrielgeneralao@gmail.com>
2023-09-18 18:37:32 -0400

    Add functionality to erase, draw, random and reset

--- js/script.js
+++ js/script.js
@@ -1,4 +1,57 @@
+let gridSize = 16;
+let penColor = "black";
+let click = false;
 const container = document.querySelector(".container");
+container.addEventListener("click", clickToggle);
+
+function clickToggle(){
+    click = !click;
+}
+
+
+const erase = document.querySelector(".erase");
+erase.addEventListener("click", eraseMode);
+
+function eraseMode(){
+    penColor = "white";
+}
+
+const draw = document.querySelector(".draw");
+draw.addEventListener("click", drawMode);
+
+function drawMode(){
+    penColor = "black";
+}
+
+const random = document.querySelector(".random");
+random.addEventListener("click", randomMode);
+
+function randomMode(){
+    penColor = "random";
+}
+
+const reset = document.querySelector(".reset");
+reset.addEventListener("click", resetMode);
+
+function resetMode(){
+    const divs = container.querySelectorAll("div");
+    divs.forEach(div => {
+        div.style.backgroundColor = "white";
+    })
+}
+
+function startPaint(){
+    if(click){
+        if (penColor === "random"){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }else{
+            this.style.backgroundColor = penColor;
+        }
+    }
+    
+}
+
+
 
 function createGrid(size){
     for(let i = 0; i < size; i++){
@@ -14,11 +67,12 @@ function createRow(size){
         for (let j = 0; j < size; j++){
             let box = document.createElement("div");
             div.append(box);
-            box.style.border = "1px solid black";
+            box.style.border = "1px solid grey";
+            box.addEventListener("mouseover", startPaint);
         }
     }
 
     
 }
 
-createRow(64)
+createRow(gridSize);
